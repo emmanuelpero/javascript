11f62c3630886f08f17037964b3d238f632b20e3
EmmanuelPero <emmap.personal@gmail.com>
2021-11-22 20:49:46 -0300

    desafio-2

---
+++ main.js
@@ -0,0 +1,11 @@
+
+let user = prompt("ingresa tu nombre");
+
+let sector = prompt("Hola " + user + " indica el sector donde trabajas");
+
+if (((user == "Emmanuel") || (user== "emmanuel")) && ((sector == "Diseño") || (sector == "diseño"))){
+    alert("Puedes ingresar al sitio");
+}else{
+    alert("No tienes acceso o ingresa los datos de nuevo");
+}
+
