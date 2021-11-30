30ea5c82e0f8af706ef59f4ee8079a80462a7e83
EmmanuelPero <emmap.personal@gmail.com>
2021-11-30 13:42:16 -0300

    Desafio clase3-Ciclos

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
+/**
+ * @challenge: descripcion del desafio
+ * 
+ * @version: nro de version
+ * @author: Emmanuel Peró
+ * @fecha: 25/11/2021
+ *
+ * History:
+ *  - v1.0.1 – Desafio Clase 1
+ *  - v1.0.2 – Correccion de nombre de variable.
+ *  - v1.0.3 – Ciclo for - lista de invitados. 
+ */
+// Código de aquí en adelante
 
-let user = prompt("ingresa tu nombre");
 
-let sector = prompt("Hola " + user + " indica el sector donde trabajas");
+// Desafio clase-2
 
-if (((user == "Emmanuel") || (user== "emmanuel")) && ((sector == "Diseño") || (sector == "diseño"))){
-    alert("Puedes ingresar al sitio");
-}else{
-    alert("No tienes acceso o ingresa los datos de nuevo");
+
+// let user = prompt("ingresa tu nombre");
+
+// let sector = prompt("Hola " + user + " indica el sector donde trabajas");
+
+// if (((user == "Emmanuel") || (user== "emmanuel")) && ((sector == "Diseño") || (sector == "diseño"))){
+//     alert("Puedes ingresar al sitio");
+// }else{
+//     alert("No tienes acceso o ingresa los datos de nuevo");
+// }
+
+
+// Desafio clase-3------------------------------------------------
+
+
+let invitados= "";
+
+for (let i = 0; i < 5; i++){
+    invitados += prompt ("Ingresa nombres de invitados") + "\n";
 }
+alert("Los invitados son: " + invitados);
+
 
